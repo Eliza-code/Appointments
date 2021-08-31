a455c022a5c55f5220af58148088cd74e544c87d
unknown <lizcano.liz14@gmail.com>
2021-08-30 19:17:18 -0500

    add a change in the store

--- src/store/index.js
+++ src/store/index.js
@@ -5,3 +5,8 @@ import rootReducer from '../reducer/index';
 const store = createStore(rootReducer , composeWithDevTools());
 
 export default store;
+
+// export const store = createStore(
+//     rootReducer, 
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//);
